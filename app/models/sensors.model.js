653027e8ad6135c42b225f264c4fbc7cba1993eb
amoda-sasmitha <amoda29@gmail.com>
2020-04-28 16:21:06 +0530

    add backend changes

--- app/models/sensors.model.js
+++ app/models/sensors.model.js
@@ -6,5 +6,5 @@ module.exports = {
     UPDATE_SENSORS : "UPDATE sensors SET smoke_level = ? , co2_level = ? , updated_at = ? WHERE id = ?; ",
     UPDATE_SENSORS_LOG : "INSERT INTO sensors_log (sensor_id, smoke_level, co2_level, datetime) VALUES ?;",
     GET_ALL_BY_MINUTES : "SELECT s.datetime , AVG(s.smoke_level) as 'average_smoke' , AVG(s.co2_level) as 'average_co2' FROM sensors_log s WHERE s.datetime >= ? AND s.datetime <= ? GROUP BY s.datetime",
-    GET_SINGLE_ALL_BY_MINUTES : "SELECT s.datetime , s.smoke_level ,s.co2_level FROM sensors_log s WHERE s.datetime >= ? AND s.datetime <= ? AND s.sensor_id = ?",
+    GET_SINGLE_ALL_BY_MINUTES : "SELECT s.id ,s.datetime , s.smoke_level ,s.co2_level FROM sensors_log s WHERE s.datetime >= ? AND s.datetime <= ? AND s.sensor_id = ?",
 };
